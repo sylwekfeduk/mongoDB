a6057ea33f6c57b507059a39486df1c2783650ca
sfeduk <sylwester.feduk@q-perior.pl>
2023-09-06 09:13:09 +0200

    fourth task

--- myApp.js
+++ myApp.js
@@ -26,11 +26,19 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({
+    name: personName
+  }, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
