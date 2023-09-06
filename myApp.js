ad98b96fd0ce1ab5e3baf51bc76e439cbdf7e339
sfeduk <sylwester.feduk@q-perior.pl>
2023-09-06 09:16:10 +0200

    fifth task

--- myApp.js
+++ myApp.js
@@ -42,7 +42,12 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({
+    favoriteFoods: food
+  }, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
